8f5722c7e18f5f9b094336912ce90f1636b75dff
Kerem Keskinoğlu <bucocuknereli@gmail.com>
2025-08-18 14:27:48 +0300

    hataları düzelttim ve küçük bir api yazdım

--- src/main/java/com/example/demo/Controller/sayhello.java
+++ src/main/java/com/example/demo/Controller/sayhello.java
@@ -2,11 +2,16 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3001")
 public class sayhello {
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello from backend";
+    public Map<String,String> hello(){
+        return Collections.singletonMap("message","Hello from backend");
     }
 }
